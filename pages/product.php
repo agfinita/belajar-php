cafab757e0b1d24134416ef00a4c183506726214
agfinita <agfinitaagh@gmail.com>
2023-10-03 15:02:24 +0700

    [STYLE] edit style product.php

--- pages/product.php
+++ pages/product.php
@@ -99,7 +99,7 @@ $products = [
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary shadow ">
         <section class="container-fluid">
-            <a class="navbar-brand"><b>FORE COFFE</b></a>
+            <a class="navbar-brand fw-bold fs-4">FORE COFFE</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -108,7 +108,7 @@ $products = [
             <section class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="login.php"> Home </a>
+                        <a class="nav-link active" aria-current="page" href="../index.php"> Home </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#product-list"> Product </a>
