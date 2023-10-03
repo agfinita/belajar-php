0b373d2ee513ddb5605cd4f4ee262d83db019564
agfinita <agfinitaagh@gmail.com>
2023-10-03 15:01:41 +0700

    [DOCS] perubahan isi file dokumen index.php

--- index.php
+++ index.php
@@ -11,76 +11,40 @@
     <title>Agfinita:belajar-php</title>
 
     <!-- favicon -->
-    <link rel="icon" type="image/png" href="../assets/img/icons8-cat-64.png">
+    <link rel="icon" type="image/png" href="assets/images/icons8-cat-64.png">
 
     <!-- bootstrap -->
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
-
-    <!-- fontawesome -->
-    <link rel="stylesheet" href="../assets/fontawesome/css/fontawesome.min.css " />
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 </head>
 
-<body>
-    <section class="min-vh-100 m-20 p-10">
-        <div class="container">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="../assets/images/books.jpg" class="img-fluid" alt="book_login">
-                </div>
-                <!-- form -->
-                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                    <form>
-                        <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                            <div class="row">
-                                <p class="col"> Sign in with </p>
-                                <!-- rencana mau dikasih logo sosmed -->
-                                <i class="fa-brands fa-google"></i>
-                                <i class="fa-brands fa-google"></i>
-                                <i class="fa-brands fa-twitter"></i>
-                                <i class="fa-brands fa-linkedin"></i>
-                            </div>
-                        </div>
+<body class="overflow-x-hidden">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow ">
+        <section class="container-fluid">
+            <a class="navbar-brand fw-bold fs-4">FORE COFFE</a>
 
-                        <div class="d-flex align-items-center my-4">
-                            <p class="text-center fw-bold mx-3 mb-0"> - Or -</p>
-                        </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                        <!-- Email input -->
-                        <div class="form-outline mb-4">
-                            <input type="email" id="form3Example3" class="form-control form-control-lg" placeholder="Enter a valid email address" />
-                            <label class="form-label" for="form3Example3">Email address</label>
-                        </div>
+            <section class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php"> Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active bg-primary text-white rounded p-1 mt-1" aria-current="page" href="pages/login.php"> Sign In </a>
+                    </li>
+                </ul>
+            </section>
+        </section>
+    </nav>
 
-                        <!-- Password input -->
-                        <div class="form-outline mb-3">
-                            <input type="password" id="form3Example4" class="form-control form-control-lg" placeholder="Enter password" />
-                            <label class="form-label" for="form3Example4">Password</label>
-                        </div>
+    <h5 class="text-center mt-4">ini halaman index</h5>
 
-                        <div class="d-flex justify-content-between align-items-center">
-                            <!-- Checkbox -->
-                            <div class="form-check mb-0">
-                                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" checked />
-                                <label class="form-check-label" for="form2Example3">
-                                    Remember me
-                                </label>
-                            </div>
-                            <a href="#!" class="text-body text-decoration-none">Forgot password?</a>
-                        </div>
-
-                        <div class="text-center text-lg-start mt-4 pt-2">
-                            <button type="button" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!" class="link-danger text-decoration-none">Register</a></p>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <!-- javascript -->
-    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../assets/fontawesome/js/fontawesome.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
