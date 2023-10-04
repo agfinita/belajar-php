e589f9e715e2f5d3b3a2a098cc9ccdb0d7f0d4df
agfinita <agfinitaagh@gmail.com>
2023-10-04 21:58:21 +0700

    [DOCS] add bootstrap framework and pages adminLTE

---
+++ pages/tables/jsgrid.php
@@ -0,0 +1,468 @@
+<?php
+// create an array to list product
+$products = [
+  [
+    'image' => '../../assets/images/productImage/americanohot173.jpg',
+    'name' => 'Hot Americano',
+    'price' => '21.000',
+    'desc' => 'Espresso shot dalam segelas cup dengan menjaga ketebalan rasa kopinya'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/americanoiced173.jpg',
+    'name' => 'Ice Americano',
+    'price' => '21.000',
+    'desc' => 'Espresso shot dalam segelas cup dengan menjaga ketebalan rasa kopinya'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/classiclatteiced173.jpg',
+    'name' => 'Ice Classic Latte',
+    'price' => '24.000',
+    'desc' => 'Perpaduan rasa espresso premium dengan saus krim spesial fore'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/matchablended173.jpg',
+    'name' => 'Matcha Ice Blended',
+    'price' => '24.000',
+    'desc' => 'Perpaduan creamy matcha khas fore coffe, susu segar dengan es'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/berrymanuka-2502.jpg',
+    'name' => 'Berry Manuka Americano',
+    'price' => '29.000',
+    'desc' => 'Perpaduan rasa stroberry dan manuka dengan classic blend fore yang menyegarkan'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/cappuccinoiced173.jpg',
+    'name' => 'Iced Cappuccino',
+    'price' => '29.000',
+    'desc' => 'Paduan espresso dengan susu sapi pilihan dan foam tebal di atasnya yang menambah kelezatan rasanya'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/caramelpralinecoffee173.jpg',
+    'name' => 'Caramel Praline Coffe Ice Blended',
+    'price' => '33.000',
+    'desc' => 'Ice blended latte dengan saus praline dan karamel yang gurih'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/salted-caramel173.jpg',
+    'name' => 'Hot Salted Caramel Mocha',
+    'price' => '33.000',
+    'desc' => 'Perpaduan coklat, latte dari house blend fore dan gurihnya caramel'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/saltedcarameliced173.jpg',
+    'name' => 'Ice Salted Caramel Mocha',
+    'price' => '33.000',
+    'desc' => 'Perpaduan coklat, latte dari house blend fore dan gurihnya caramel yang dingin'
+  ],
+
+  [
+    'image' => '../../assets/images/productImage/Nutty_Oat_Latte.jpeg',
+    'name' => 'Nutty Oat Latte',
+    'price' => '39.000',
+    'desc' => 'Espresso dari biji kopi khas nusantara dipadukan susu oat gluten-free dan sensasi nutty dari huzelnut'
+  ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Admin | Product</title>
+
+  <!--favicon-->
+  <link rel="icon" type="image/png" href="../../assets/images/icons8-cat-64.png" />
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../assets/plugins/fontawesome-free/css/all.min.css" />
+  <!-- jsGrid -->
+  <link rel="stylesheet" href="../../assets/plugins/jsgrid/jsgrid.min.css" />
+  <link rel="stylesheet" href="../../assets/plugins/jsgrid/jsgrid-theme.min.css" />
+  <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css" />
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../assets/dist/css/adminlte.min.css" />
+</head>
+
+<body class="hold-transition sidebar-mini">
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="../../index.html" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Contact</a>
+        </li>
+      </ul>
+
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Navbar Search -->
+        <li class="nav-item">
+          <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <i class="fas fa-search"></i>
+          </a>
+          <div class="navbar-search-block">
+            <form class="form-inline">
+              <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" />
+                <div class="input-group-append">
+                  <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                  </button>
+                  <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </li>
+
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="far fa-comments"></i>
+            <span class="badge badge-danger navbar-badge">3</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <a href="#" class="dropdown-item">
+              <!-- Message Start -->
+              <div class="media">
+                <img src="../../assets/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle" />
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    Brad Diesel
+                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                  </h3>
+                  <p class="text-sm">Call me whenever you can...</p>
+                  <p class="text-sm text-muted">
+                    <i class="far fa-clock mr-1"></i> 4 Hours Ago
+                  </p>
+                </div>
+              </div>
+              <!-- Message End -->
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <!-- Message Start -->
+              <div class="media">
+                <img src="../../assets/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3" />
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    John Pierce
+                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                  </h3>
+                  <p class="text-sm">I got your message bro</p>
+                  <p class="text-sm text-muted">
+                    <i class="far fa-clock mr-1"></i> 4 Hours Ago
+                  </p>
+                </div>
+              </div>
+              <!-- Message End -->
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <!-- Message Start -->
+              <div class="media">
+                <img src="../../assets/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3" />
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    Nora Silvester
+                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                  </h3>
+                  <p class="text-sm">The subject goes here</p>
+                  <p class="text-sm text-muted">
+                    <i class="far fa-clock mr-1"></i> 4 Hours Ago
+                  </p>
+                </div>
+              </div>
+              <!-- Message End -->
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+          </div>
+        </li>
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="far fa-bell"></i>
+            <span class="badge badge-warning navbar-badge">15</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <span class="dropdown-item dropdown-header">15 Notifications</span>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-envelope mr-2"></i> 4 new messages
+              <span class="float-right text-muted text-sm">3 mins</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-users mr-2"></i> 8 friend requests
+              <span class="float-right text-muted text-sm">12 hours</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-file mr-2"></i> 3 new reports
+              <span class="float-right text-muted text-sm">2 days</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+            <i class="fas fa-th-large"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="#" class="brand-link">
+        <img src="../../assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
+        <span class="brand-text font-weight-light">Admin Store</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="../../assets/images/profile_2.jpg" class="img-circle elevation-2" alt="User Image" />
+          </div>
+          <div class="info">
+            <a href="#" class="d-block">Agfinita Gusti Hikmawani</a>
+          </div>
+        </div>
+
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+          <div class="input-group" data-widget="sidebar-search">
+            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
+            <div class="input-group-append">
+              <button class="btn btn-sidebar">
+                <i class="fas fa-search fa-fw"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                  Dashboard
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="../dashboard.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Dashboard</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="nav-item menu-open">
+              <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-table"></i>
+                <p>
+                  Tables
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="jsgrid.php" class="nav-link active">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Product</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="nav-header">OTHERS</li>
+            <li class="nav-item">
+              <a href="../calendar.php" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                  Calendar
+                  <span class="badge badge-info right">2</span>
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-book"></i>
+                <p>
+                  Pages
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="../examples/contacts.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Contacts</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-search"></i>
+                <p>
+                  Search
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="../search/enhanced.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Enhanced</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>Product</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item">
+                  <a href="#">Home</a>
+                </li>
+                <li class="breadcrumb-item active">Product</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+        <!-- /.container-fluid -->
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="card card-blue">
+          <div class="card-header">
+            <h3 class="card-title">Products</h3>
+          </div>
+          <!-- /.card-header -->
+
+          <div>
+            <!-- listing product will display in here -->
+            <section class="container-fluid">
+              <section class=" mx-3 mt-5" id="product-list">
+                <section class="row row-cols-1 row-cols-md-2 g-2">
+                  <?php foreach ($products as $product) : ?>
+                    <section class="col">
+                      <section class="card mb-3 p-2 " style="max-width: 550px;">
+                        <section class="row g-0">
+                          <!-- image product -->
+                          <picture class="col-md-4 mt-3">
+                            <img src="<?php echo $product['image']; ?>" alt="product_image" class="img-fluid rounder-start" alt="">
+                          </picture>
+
+                          <section class="col-md-8">
+                            <section class="card-body">
+                              <h5 class="card-title"> <?php echo $product['name']; ?> </h5>
+                              <p class="card-text"> <?php echo 'Rp' . $product['price']; ?> </p>
+                              <p class="card-text"> <?php echo $product['desc']; ?> </p>
+                              <button type="button" class="btn btn-primary"> <?php echo 'Buy Now'; ?> </button>
+                            </section>
+                          </section>
+                        </section>
+                      </section>
+                    </section>
+                  <?php endforeach; ?>
+                </section>
+              </section>
+            </section>
+          </div>
+        </div>
+        <!-- /.card -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-block"><b>Version</b> 3.2.0</div>
+      <strong>Copyright &copy; 2014-2021
+        <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+      All rights reserved.
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="../../assets/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- jsGrid -->
+  <script src="../../assets/plugins/jsgrid/demos/db.js"></script>
+  <script src="../../assets/plugins/jsgrid/jsgrid.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../assets/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="../../assets/dist/js/demo.js"></script>
+  <!-- Page specific script -->
+  <script src="../../assets/bootstrap/js/bootstrap.min.js">
+  </script>
+</body>
+
+</html>
